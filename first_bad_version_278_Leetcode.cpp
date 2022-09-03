f49c9367fac782c2027a14ba132115886ce20cc3
bee-llel <billelhakem@gmail.com>
2022-09-03 20:25:18 +0200

    Done

---
+++ first_bad_version_278_Leetcode.cpp
@@ -0,0 +1,26 @@
+// Leetcode 278. First bad version 
+
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+#include <typeinfo>
+#include <type_traits>
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        
+       int bad = -1;
+        
+        // if (n == 1) return 1;
+        
+        for(int b = n/2 +1; b >= 1; b /= 2){
+            while(!isBadVersion(bad + b)){
+                bad += b;
+            }
+        }
+        int k = bad+1;
+        return k;
+        
+    }
+};
